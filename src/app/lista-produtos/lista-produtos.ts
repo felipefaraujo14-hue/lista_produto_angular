38a625858603cee8eb658efde176383fc33b4c43
Felipe <felipe.f.araujo14@aluno.senai.br>
2026-07-30 16:38:44 -0300

    ....

--- src/app/lista-produtos/lista-produtos.ts
+++ src/app/lista-produtos/lista-produtos.ts
@@ -10,23 +10,31 @@ import { Item } from './item';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-
-  id_produto : number = 0
-  descricao_produto :string = ''
-  valor_unitario : number = 0.0
+  id_produto: number = 0;
+  descricao_produto: string = '';
+  valor_unitario: number = 0.0;
 
   listaItens: Item[] = [];
 
   addItem() {
-
-    let item = new Item();
-    item.idProduto = this.listaItens.length + 1;
-    item.descricaoProduto = this.descricao_produto;
-    item.valorUnitario = this.valor_unitario;
-
+    if (!this.descricao_produto.trim()) return;
+  
+    const item: Item = {
+      idProduto: this.listaItens.length + 1,
+      descricaoProduto: this.descricao_produto,
+      valorUnitario: this.valor_unitario,
+      statusSelecionado: false 
+    };
+  
     this.listaItens.push(item);
+  
+    this.descricao_produto = '';
+    this.valor_unitario = 0.0;
+  }
 
-    this.descricao_produto = ''
-    this.valor_unitario = 0.0
+  limpaForm() {
+    this.listaItens = [];
+    this.descricao_produto = '';
+    this.valor_unitario = 0.0;
   }
 }
